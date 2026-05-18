baa52035d8619c16102b6b8b203c180847e900cf
levi <levi.serrano1204@gmail.com>
2026-05-18 15:26:01 -0300

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> pilha;
+
+        for (const char& c : s) {
+            if (c == '{') pilha.push('}');
+            else if (c == '(') pilha.push(')');
+            else if (c == '[') pilha.push(']');
+
+            else {
+                if (pilha.empty() || c != pilha.top()) return false;
+                pilha.pop();
+            }
+       }
+       return pilha.empty();
+    }
+};
