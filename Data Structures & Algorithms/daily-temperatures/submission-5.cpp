ff8767b7553e379f9c143cb680489a7429265bf3
levi <levi.serrano1204@gmail.com>
2026-05-18 17:41:01 -0300

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int, int>> aux;
+
+        const int n = temperatures.size();
+        vector<int> res(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            int curr = temperatures[i];
+            while (!aux.empty() && curr > aux.top().first) {
+                int i_topo = aux.top().second;
+                res[i_topo] = i - i_topo;
+                aux.pop();
+            }
+            aux.push({curr, i});
+        }
+        return res;
+    }
+};
