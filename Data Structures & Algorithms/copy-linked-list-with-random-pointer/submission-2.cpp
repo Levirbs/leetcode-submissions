d086cad3186c19d2fb4752dc3cff14b35844ab07
levi <levi.serrano1204@gmail.com>
2026-05-30 18:17:57 -0300

    Add: copy-linked-list-with-random-pointer - submission-2

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-2.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+        
+        Node* node = head;
+        while (node) {
+            Node* aux = node->next;
+            node->next = new Node(node->val);
+            node->next->next = aux;
+            node = aux;
+        }
+
+        node = head;
+        while (node) {
+            if (node->random) node->next->random = node->random->next;
+            node = node->next->next;
+        }
+
+        node = head;
+        Node* res = head->next;
+        while (node) {
+            Node* aux = node->next;
+            node->next = aux->next;
+            if (aux->next) aux->next = aux->next->next;
+            node = node->next;
+        }
+        return res;
+    }
+};
