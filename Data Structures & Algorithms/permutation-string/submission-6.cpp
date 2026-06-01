3fe0ef210cbe3e29a6d433961b28d1c93e3f2e98
levi <levi.serrano1204@gmail.com>
2026-06-01 11:01:52 -0300

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        if (n1 > n2) return false;
+
+        vector<int> word(26, 0);
+        vector<int> window(26, 0);
+
+        for (int i = 0; i < n1; i++) {
+            word[s1[i] - 'a']++;
+            window[s2[i] - 'a']++;
+        }
+
+        int matches = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (word[i] == window[i]) matches++;
+        }
+
+        int l = 0;
+        for (int r = n1; r < n2; r++) {
+            if (matches == 26) return true;
+
+            int c = s2[r] - 'a';
+            window[c]++;
+            if (window[c] == word[c]) {
+                matches++;
+            } else if (window[c] - 1 == word[c]) {
+                matches--;
+            }
+
+            c = s2[l] - 'a';
+            window[c]--;
+            if (window[c] == word[c]) {
+                matches++;
+            } else if (window[c] + 1 == word[c]) {
+                matches--;
+            }
+            l++;
+        }
+
+        return matches == 26;
+    }
+};
