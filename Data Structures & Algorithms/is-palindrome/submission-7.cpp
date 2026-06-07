4e03b1bc0893dabd3437a5cbef31d10aaaa70bbc
levi <levi.serrano1204@gmail.com>
2026-06-07 12:40:04 -0300

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        // isalnum()
+        // tolower()
+        int n = s.length();
+        int l = 0;
+        int r = n - 1;
+        while (l < r) {
+            while (!isalnum(s[l]) && l < r) l++;
+            char cleft = tolower(s[l]);
+
+            while (!isalnum(s[r]) && l < r) r--;
+            char cright = tolower(s[r]);
+
+            if (cleft != cright) return false;
+            l++; r--;
+        }
+
+        return true;
+    }
+};
