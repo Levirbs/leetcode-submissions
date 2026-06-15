eafa4978bed3571150cc81b1b8b1a51315b1a5a5
levi <levi.serrano1204@gmail.com>
2026-06-15 10:57:53 -0300

    Add: merge-k-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-3.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+private:
+    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+        ListNode dummy;
+        ListNode* node = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                node->next = l1;
+                l1 = l1->next;
+            } else {
+                node->next = l2;
+                l2 = l2->next;
+            }
+            node = node->next;
+        }
+
+        if (l1) {
+            node->next = l1;
+        } else if (l2) {
+            node->next = l2;
+        }
+
+        return dummy.next;
+    } 
+
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return nullptr;
+
+        ListNode* res = lists[0];
+
+        int n = lists.size();
+
+        for (int i = 1; i < n; i++) {
+            res = merge2Lists(res, lists[i]);
+        }
+
+        return res;
+    }
+};
