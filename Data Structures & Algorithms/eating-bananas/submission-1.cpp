627d1d8264fee251b4498d47dcfc9e0f43a65928
levi <levi.serrano1204@gmail.com>
2026-06-15 23:36:31 -0300

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int r = 0;
+
+        for (const int& pile : piles) r = max(r, pile);
+
+        int res = r;
+
+        while (l <= r) {
+            int k = (l + r) / 2;
+
+            int time = 0;
+            for (int pile : piles) {
+                time += ceil(static_cast<double>(pile) / k);
+            }
+
+            if (time <= h) {
+                res = k;
+                r = k - 1;
+            } else {
+                l = k + 1;
+            }
+        }
+
+        return res;
+    }
+};
