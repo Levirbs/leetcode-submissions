1846077c00efad298d058c69edf754f385c9ad84
levi <levi.serrano1204@gmail.com>
2026-07-05 11:30:58 -0300

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> mapa;
+
+        for (const int num : nums) {
+            if (mapa.count(num)) return true;
+            mapa.insert(num);
+        }
+
+        return false;
+    }
+};
