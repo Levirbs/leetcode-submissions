0b55667c99685aa3c1d058a91a7a2feb02e73930
levi <levi.serrano1204@gmail.com>
2026-07-05 11:51:55 -0300

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mapa;
+
+        for (const string& word : strs) {
+            vector<int> count(26, 0);
+            for (const char& c : word) {
+                count[c - 'a']++;
+            }
+
+            string key;
+            for (const int& i : count) {
+                key += to_string(i) + "#";
+            }
+
+            mapa[key].push_back(word);
+        }
+
+        vector<vector<string>> res;
+
+        for (const auto& pair : mapa) {
+            res.push_back(pair.second);
+        }
+
+        return res;
+    }
+};
