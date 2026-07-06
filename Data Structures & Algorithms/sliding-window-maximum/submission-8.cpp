664fe0e45dc6a30b2ce0d101f53d03b2311b3907
levi <levi.serrano1204@gmail.com>
2026-07-06 18:09:06 -0300

    Add: sliding-window-maximum - submission-8

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-8.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> res(n - k + 1);
+
+        deque<int> fila;
+
+        int l = 0;
+        for (int r = 0; r < n; r++) {
+            int num = nums[r];
+            while (!fila.empty() && num > nums[fila.back()]) fila.pop_back();
+            fila.push_back(r);
+
+            if (fila.front() < l) fila.pop_front();
+
+            if (r + 1 >= k) {
+                res[l] = nums[fila.front()];
+                l++;
+            }
+        }
+        return res;
+    }
+};
