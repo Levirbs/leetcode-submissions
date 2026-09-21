5da986fdc8f5e2e22c9a968af10d960ee5742b26
levi <levi.serrano1204@gmail.com>
2026-09-21 12:04:58 -0300

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.cpp
@@ -0,0 +1,40 @@
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<string, int>>> mapa;
+
+public:
+    TimeMap() {
+        mapa.clear();
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mapa[key].push_back({value, timestamp});
+    }
+    
+    string get(string key, int timestamp) {
+        if (!mapa.count(key)) {
+            return "";
+
+        } else {
+            auto& lista = mapa[key];
+            
+            int l = 0;
+            int r = lista.size() - 1;
+
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+                auto& par = lista[m];
+
+                if (par.second == timestamp) return par.first;
+
+                if (par.second < timestamp) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+
+            return r >= 0 ? lista[r].first : "";
+        }
+    }
+};
