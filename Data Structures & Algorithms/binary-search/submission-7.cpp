75a23693fa1a8ae2695ffc64c7445a24b60809cd
levi <levi.serrano1204@gmail.com>
2026-09-21 10:12:39 -0300

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        int l = 0;
+        int r = n - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            int num = nums[m];
+
+            if (num == target) return m;
+
+            if (num > target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return -1;
+    }
+};
