4fa68cc1d5f88ce95205ef6cabee36f7cf30221f
levi <levi.serrano1204@gmail.com>
2026-09-23 21:15:23 -0300

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> count(26, 0);
+        for (const char& task : tasks) count[task - 'A']++;
+
+        priority_queue<int> maxHeap;
+        for (const int& cnt : count) if (cnt > 0) maxHeap.push(cnt);
+
+        queue<pair<int, int>> fila;
+        int time = 0;
+
+        while (!maxHeap.empty() || !fila.empty()) {
+            time++;
+            
+            if (!maxHeap.empty()) {
+                int cnt = maxHeap.top() - 1; maxHeap.pop();
+
+                if (cnt > 0) fila.push({cnt, time + n});
+            } else {
+                time = fila.front().second;
+            }
+
+            if (!fila.empty() && fila.front().second == time) {
+                maxHeap.push(fila.front().first); fila.pop();
+            }
+        }
+
+        return time;
+    }
+};
